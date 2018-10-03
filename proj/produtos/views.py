839b628a728eb3645e20e114d84a3be69c163aad
Felipe Augusto Bento Albuquerque <felipeaugustobento@hotmail.com>
2018-10-02 23:49:31 -0300

    first commit

---
+++ proj/produtos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Produto
+
+# Create your views here.
+
+def home(request):
+    nome = 'Django MOC'
+    produtos = Produto.objects.all()
+    # produtos = Produto.objects.get(id=1)
+    return render(request, 'produtos.html', {'produtos': produtos})
+
+def produto(request, codigo):
+    produto = Produto.objects.get(id=codigo)
+    return render(request, 'produtos.html', {'produto': produto})
